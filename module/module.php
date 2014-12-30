9f55ad228b84b3729338a712b69b14e3c4ef4072
chaomaogithub <chaomaochina88@163.com>
2014-12-30 16:06:36 +0800

    modify some files

--- module/module.php
+++ module/module.php
@@ -2,7 +2,7 @@
 session_start();
 require_once("../includes/db.php");
 $db = new DB();
-$act = $_POST["act"];
+$act = $_GET["act"];
 
 switch ($act) {
 	case "del":
@@ -23,16 +23,16 @@ switch ($act) {
 }
 
 /*
-* 列出文章数据
+* 列出杯赛数据
 * $db object 数据库连接对象
 * $id 查询的分类ID
 */
 function listData($db, $id = null) {
 	if (empty($id)) {
-		$sql = "select id, title, time from match";
+		$sql = "select id, title, time from cup";
 		$data = $db->get_all($sql);
 	} else {
-		$sql = "select * from article where id = ".$id;
+		$sql = "select * from cup where id = ".$id;
 		$data = $db->get_one($sql);
 	}
 
@@ -45,34 +45,31 @@ function listData($db, $id = null) {
 }
 
 /*
-* 更新文章，包括增加和修改
+* 更新杯赛，包括增加和修改
 * $db object 数据库连接对象
 * $act string 操作类型
 */
 function mod ($db, $act) {
 	$title = $_POST["title"];
-	$summary = $_POST["summary"];
-	$sort = $_POST["sort"];
-	$thumbnails = $_POST["thumbnails"];
-	$content = $_POST["content"];
-	$seotitle = $_POST["seotitle"];
-	$seokeyword = $_POST["seokeyword"];
-	$seodesc = $_POST["seodesc"];
+	$start = $_POST["start"];
+	$end = $_POST["end"];
+	$type = $_POST["type"];
+	$num = $_POST["num"];
+	$desc = $_POST["desc"];
+	$reward = $_POST["reward"];
 
 	$data = array(
 		"title" => $title,
-		"summary" => $summary,
-		"sort" => $sort,
-		"thumbnails" => $thumbnails,
-		"author" => $author,
-		"content" => $content,
-		"time" => date('Y-m-d H:i:s',time()),
-		"seotitle"=> $seotitle,
-		"seokeyword" => $seokeyword,
-		"seodesc" => $seodesc
+		"start" => $start,
+		"end" => $end,
+		"type" => $type,
+		"num" => $num,
+		"desc" => $desc,
+		"reward" => $reward,
+		"time" => date('Y-m-d H:i:s',time())
 	);
 	if ($act === "add") {
-		$data["author"] = $_SESSION['username'];
+		$data["author"] = $_SESSION["uid"];
 		add($db, $data);
 	} else {
 		edit($db, $data, $id);
@@ -80,47 +77,47 @@ function mod ($db, $act) {
 }
 
 /*
-* 发布文章
+* 增加杯赛
 * $db object 数据库连接对象
 * $data array 数据对象
 */
 function add ($db, $data) {
-	$result = $db->insert("article", $data);
+	$result = $db->insert("cup", $data);
 	if ($result) {
-		$result = array("code"=> "ok", "desc"=> "发布文章成功！");
+		$result = array("code"=> "ok", "desc"=> "发布杯赛成功！");
 	} else {
-		$result = array("code"=> "error", "desc"=> "发布文章失败！");
+		$result = array("code"=> "error", "desc"=> "发布杯赛失败！");
 	}
 	echo json_encode($result);
 }
 
 /*
-* 修改文章
+* 修改杯赛
 * $db object 数据库连接对象
 * $data array 数据对象
 * $id number 需要修改的id
 */
 function edit ($db, $data, $id) {
-	$result = $db->update("article", $data, "id=".$id);
+	$result = $db->update("cup", $data, "id=".$id);
 	if ($result) {
-		$result = array("code"=> "ok", "desc"=> "修改文章成功！");
+		$result = array("code"=> "ok", "desc"=> "修改杯赛成功！");
 	} else {
-		$result = array("code"=> "error", "desc"=> "修改文章失败！");
+		$result = array("code"=> "error", "desc"=> "修改杯赛失败！");
 	}
 	echo json_encode($result);
 }
 
 /*
-* 删除文章
+* 删除杯赛
 * $db object 数据库连接对象
 * $id number  分类ID
 */
-function del($db, $id) {
-	$result = $db->delete("article", "id in (".$id.")");
+function del ($db, $id) {
+	$result = $db->delete("cup", "id in (".$id.")");
 	if ($result) {
-		$result = array("code"=> "ok", "desc"=> "删除文章成功！");
+		$result = array("code"=> "ok", "desc"=> "删除杯赛成功！");
 	} else {
-		$result = array("code"=> "error", "desc"=> "删除文章失败！");
+		$result = array("code"=> "error", "desc"=> "删除杯赛失败！");
 	}
 	echo json_encode($result);
 }
