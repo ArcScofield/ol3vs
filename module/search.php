fe1927ebd157055825482c67d7ce3376dbd3aa16
chaomaogithub <chaomaochina88@163.com>
2015-02-15 17:24:29 +0800

    modify some files

--- module/search.php
+++ module/search.php
@@ -1,5 +1,9 @@
 <?php
+require_once("../includes/db.php");
+$db = new DB();
+
 // 检查是否直接访问本页面
+/*
 if( isset($_SERVER['HTTP_REFERER']) ) {
     $url_array = explode('http://', $_SERVER['HTTP_REFERER']);
     $url = explode('/', $url_array[1]);
@@ -9,6 +13,44 @@ if( isset($_SERVER['HTTP_REFERER']) ) {
 } else {
 	exit('Access Denied! Please do not load this page directly.');
 }
+*/
 
-print_r($_POST["name"]);
+$act = $_GET["act"];
+
+if ($act === "list") {
+	$pos = $_POST["pos"];
+	$ls = $_POST["ls"];
+	$sj = $_POST["sj"];
+	$hsdz = $_POST["hsdz"];
+
+	$sql = "select player.sj, player.pos, player.gj, player.zp, player.jlb, player.name, hashid from player";
+	if (isset($pos)) {
+		$sql = $sql." pos='".$pos."'";
+	}
+	if (isset($ls)) {
+		$sql = $sql." ls=".$ls;
+	}
+	if (isset($sj)) {
+		$sql = $sql." sj='".$sj."'";
+	}
+
+	$data = $db->get_all($sql);
+
+	if (is_array($data)) {
+		$result = array("code"=> "ok", "desc"=> "读取数据成功！", "content"=> $data);
+	} else {
+		$result = array("code"=> "error", "desc"=> "读取数据失败！");
+	}
+	echo json_encode($result);
+} else if ($act === "xx") {
+	$sql = "select * from cup where id = ".$id;
+	$data = $db->get_one($sql);
+
+	if (is_array($data)) {
+		$result = array("code"=> "ok", "desc"=> "读取数据成功！", "content"=> $data);
+	} else {
+		$result = array("code"=> "error", "desc"=> "读取数据失败！");
+	}
+	echo json_encode($result);
+}
 ?>
